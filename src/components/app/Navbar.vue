da6840b125b5a6dcd14e4b5ff1594165b59c5fae
e.pimenov <e.pimenov@bars.group>
2022-07-27 12:54:52 +0300

    lesson 4 - navbar date and dropdown

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -6,7 +6,7 @@
         <a href="#" @click.prevent="$emit('toggle')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ date }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -15,6 +15,7 @@
             class="dropdown-trigger black-text"
             href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -22,13 +23,15 @@
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
-                <i class="material-icons">account_circle</i>Профиль
-              </a>
+              <router-link to="/profile">
+                <a href="#" class="black-text">
+                  <i class="material-icons">account_circle</i>Профиль
+                </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -38,3 +41,37 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null,
+  }),
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date =
+        new Date().toLocaleTimeString() + " " + new Date().toLocaleDateString();
+    }, 1000);
+    this.dropdown = window.M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: true,
+    });
+  },
+  beforeUnmount() {
+    /* нужно для того, чтобы не происходили утечки памяти.
+    так  интервал и Dropdown будут жить после unmount, 
+    то их необходимо удалить*/
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+  methods: {
+    logout() {
+      console.log("logout");
+      this.$router.push("/login?message=logout");
+    },
+  },
+};
+</script>
--- src/layouts/mainLayout.vue
+++ src/layouts/mainLayout.vue
@@ -14,9 +14,11 @@
     </main>
 
     <div class="fixed-action-btn">
-      <a class="btn-floating btn-large blue" href="#">
-        <i class="large material-icons">add</i>
-      </a>
+      <router-link to="/record">
+        <a class="btn-floating btn-large blue" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </router-link>
     </div>
   </div>
 </template>
